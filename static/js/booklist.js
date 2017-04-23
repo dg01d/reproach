303ad600c3fa38ab0cf7aea62e80678a32f9b5e6
Daniel Goldsmith <dgold@ascraeus.org>
2017-04-23 18:46:33 +0100

    final tbr list version

--- static/js/booklist.js
+++ static/js/booklist.js
@@ -1,17 +1,4 @@
 $.getJSON('/bookdb/db.json', function(data) {
-	var reading = "<div class=\"curr-cont\">"
-	for (var i in data.books) {
-		if (data.books[i].amReading == "true" || data.books[i].amReading == true) {
-			
-			reading += "<div class=\"curr-img\"><img width=\"80\" height=\"120\" src=\"http://books.google.com/books/content?id=" + data.books[i].id + "&printsec=frontcover&img=1&zoom=5&source=gbs_api\"></div>"
-			
-			reading += "<div class=\"cssProgress curr-prog\"><div class=\"centre\">I am on page " + Math.floor(data.books[i].numPages *(data.progress.read / 100)) + " of <em>" + data.books[i].title + "</em> by "  + data.books[i].author + "<div class=\"progress2\"><div class=\"cssProgress-bar\" data-percent=\"" + data.progress.read + "\" style=\"width: " + data.progress.read + "%;\"><span class=\"cssProgress-label\">" + data.progress.read + "%</span></div></div><div class=\"centre\"><em>\"" + data.books[i].abstract + "\"</em></div>"
-			}
-	document.getElementById("current").innerHTML=reading;
-	}
-});
-
-$.getJSON('/bookdb/db.json', function(data) {
 	var output = "";
 	for (var i in data.books) {
 		if (data.books[i].amReading == "false" || data.books[i].amReading == false) {
@@ -19,7 +6,7 @@ $.getJSON('/bookdb/db.json', function(data) {
 				output += "<div class=\"tbr-entry\">";
 				output += "<div class=\"tbr-img\"><img width=\"80\" height=\"120\" src=\"http://books.google.com/books/content?id=" + data.books[i].id + "&printsec=frontcover&img=1&zoom=5&source=gbs_api\"></div>";
 				output += "<div class=\"tbr-item\"><div class=\"centre\"><em>" + data.books[i].title + "</em> by "  + data.books[i].author + "<br /><em>\"" + data.books[i].abstract + "\"</em></div></div>";
-				output += "</div>";
+				output += "</div><hr />";
 			}
 		}
 	document.getElementById("booklist").innerHTML=output;
