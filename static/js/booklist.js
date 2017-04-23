b87754a1ca3f633ada2061f7daf53aa84bcda2fb
Daniel Goldsmith <dgold@ascraeus.org>
2017-04-23 18:59:04 +0100

    fix

--- static/js/booklist.js
+++ static/js/booklist.js
@@ -4,7 +4,7 @@ $.getJSON('/bookdb/db.json', function(data) {
 		if (data.books[i].amReading == "false" || data.books[i].amReading == false) {
 			if (data.books[i].myDate == " ") {
 				output += "<div class=\"tbr-entry\">";
-				output += "<div class=\"tbr-img\"><img width=\"80\" height=\"120\" src=\"http://books.google.com/books/content?id=" + data.books[i].id + "&printsec=frontcover&img=1&zoom=5&source=gbs_api\"></div>";
+				output += "<div class=\"tbr-img\"><img width=\"80\" height=\"120\" src=\"https://books.google.com/books/content?id=" + data.books[i].id + "&printsec=frontcover&img=1&zoom=5&source=gbs_api\"></div>";
 				output += "<div class=\"tbr-item\"><div class=\"centre\"><em>" + data.books[i].title + "</em> by "  + data.books[i].author + "<br /><em>\"" + data.books[i].abstract + "\"</em></div></div>";
 				output += "</div><hr />";
 			}
