d6639235512b12174ae169da140bdd7392f6c4ae
Daniel Goldsmith <dgold@ascraeus.org>
2017-04-23 17:18:25 +0100

    need to remove the db file

--- static/js/booklist.js
+++ static/js/booklist.js
@@ -12,11 +12,17 @@ $.getJSON('/bookdb/db.json', function(data) {
 });
 
 $.getJSON('/bookdb/db.json', function(data) {
-	var output = "<ul>";
+	var output = "";
 	for (var i in data.books) {
-		output += "<li>" + data.books[i].title + "</li>";
-	}
-	output += "</ul>";
+		if (data.books[i].amReading == "false" || data.books[i].amReading == false) {
+			if (data.books[i].myDate == " ") {
+				output += "<div class=\"tbr-entry\">";
+				output += "<div class=\"tbr-img\"><img width=\"80\" height=\"120\" src=\"http://books.google.com/books/content?id=" + data.books[i].id + "&printsec=frontcover&img=1&zoom=5&source=gbs_api\"></div>";
+				output += "<div class=\"tbr-item\"><div class=\"centre\"><em>" + data.books[i].title + "</em> by "  + data.books[i].author + "<br /><em>\"" + data.books[i].abstract + "\"</em></div></div>";
+				output += "</div>";
+			}
+		}
 	document.getElementById("booklist").innerHTML=output;
+	}
 });
 
