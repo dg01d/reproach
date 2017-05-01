2bc0e1c4dc7df620f2a2a45f29b0c604faa33f06
Daniel Goldsmith <dgold@ascraeus.org>
2017-05-01 18:38:29 +0100

    Three fixes for this release
    1. Fixed the div handling of the current reading
    2. Fixed the height issues on current reading on mobile devices
    3. Fixed the errant link to reading page.

--- static/js/bookcurrent.js
+++ static/js/bookcurrent.js
@@ -1,11 +1,13 @@
 $.getJSON('/bookdb/db.json', function(data) {
-	var reading = "<div class=\"curr-cont\">"
+	var reading = "<div class=\"curr-cont h-entry\">"
 	for (var i in data.books) {
 		if (data.books[i].amReading == "true" || data.books[i].amReading == true) {
 			
-			reading += "<div class=\"curr-img h-entry\"><img width=\"80\" height=\"120\" src=\"https://books.google.com/books/content?id=" + data.books[i].id + "&printsec=frontcover&img=1&zoom=5&source=gbs_api\"></div>"
+			reading += "<div class=\"curr-img\"><img width=\"80\" height=\"120\" src=\"https://books.google.com/books/content?id=" + data.books[i].id + "&printsec=frontcover&img=1&zoom=5&source=gbs_api\"></div>"
 			
-			reading += "<div class=\"cssProgress curr-prog\"><div class=\"centre e-content\"><strong>I am on page " + Math.floor(data.books[i].numPages *(data.progress.read / 100)) + " of <em>" + data.books[i].title + "</em> by "  + data.books[i].author + "</strong><div class=\"progress2\"><div class=\"cssProgress-bar\" data-percent=\"" + data.progress.read + "\" style=\"width: " + data.progress.read + "%;\"><span class=\"cssProgress-label\">" + data.progress.read + "%</span></div></div><div class=\"centre\"><em>\"" + data.books[i].abstract + "\"</em></div>"
+			reading += "<div class=\"cssProgress curr-prog\"><div class=\"centre e-content\"><strong>I am on page " + Math.floor(data.books[i].numPages *(data.progress.read / 100)) + " of <em>" + data.books[i].title + "</em> by "  + data.books[i].author + "</strong><div class=\"progress2\"><div class=\"cssProgress-bar\" data-percent=\"" + data.progress.read + "\" style=\"width: " + data.progress.read + "%;\"><span class=\"cssProgress-label\">" + data.progress.read + "%</span></div></div><div class=\"centre\"><em>\"" + data.books[i].abstract + "\"</em></div></div></div>"
+			reading += "</div></div><hr />"
+
 			}
 	document.getElementById("current").innerHTML=reading;
 	}
